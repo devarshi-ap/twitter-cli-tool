1a6db24bc954c4618e2d5f1e4a013fe9b7d4ae9d
dev <devarshi.ap@gmail.com>
2021-10-20 00:15:33 -0400

    swap api dummy keys

--- twitterCli.py/tcli.py
+++ twitterCli.py/tcli.py
@@ -11,3 +11,24 @@
 # by 2021 Devarshi Patel <devarshi.patel@ryerson.ca>, Toronto
 # Released under (C) WTFPL License
 # -----------------------------------------------------------
+
+import tweepy
+import click
+
+# library versions
+
+print(f'tweepy version: {tweepy.__version__}\nclick version: {click.__version__}\n')
+
+# tweepy authentication tokens
+
+consumer_key = 'xxxxxxxxxxxxx'
+consumer_secret = 'xxxxxxxxxxxxx'
+access_token = 'xxxxxxxxxxxxx'
+access_token_secret = 'xxxxxxxxxxxxx'
+
+# Authentication
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+MY_USERNAME = 'your_username_here_without_@'
