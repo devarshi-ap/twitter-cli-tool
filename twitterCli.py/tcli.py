5d9b8a73e35576dab7592c558d101ae01f5b538a
dev <devarshi.ap@gmail.com>
2021-10-20 00:11:22 -0400

    project introduction 👋

---
+++ twitterCli.py/tcli.py
@@ -0,0 +1,13 @@
+# TWITTER COMMAND LINE INTERFACE TOOL
+#
+# CLI supports the following methods:
+#   - List 'followers' and 'following' of given user
+#   - Tweet
+#   - Delete Tweets
+#   - Follow and Unfollow users
+#   - Filter (Delete) tweets based on specific words (don't get cancelled for past offensive tweets)
+#
+#
+# by 2021 Devarshi Patel <devarshi.patel@ryerson.ca>, Toronto
+# Released under (C) WTFPL License
+# -----------------------------------------------------------
